40b32307a286e2c51c2d237cf1ae1628abd3d55d
Mike Funk <mike@mikefunk.com>
2015-05-15 10:52:04 -0700

    working

--- src/app.js
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-// var TodoApp = React.createFactory(require('./components/TodoApp.jsx'));
-var TodoApp = require('./components/TodoApp.jsx');
-
-React.render(
-  React.createElement(TodoApp, {url: "http://jsonplaceholder.typicode.com/todos"}),
-  document.getElementById('app')
-);
