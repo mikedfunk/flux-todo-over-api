87e711214ce8095b0522345ff1db2e1f81f975d6
Mike Funk <mike@mikefunk.com>
2015-05-15 17:54:56 -0700

    update now works

--- lib/Todo.jsx
+++ lib/Todo.jsx
@@ -8,16 +8,45 @@
    * an individual todo element. key is assigned when invoking this from a parent.
    */
   var Todo = React.createClass({
-    // pass click of delete button to the action, which the store listens to
+    /**
+     * pass click of delete button to the action, which the store listens to
+     * @param {object} event
+     */
     handleDeleteClick: function (e) {
       e.preventDefault();
       TodoActions.deleteTodo(this.props.todo.id);
     },
+    /**
+     * when clicking a todo text to edit it
+     * @param {object} event
+     */
+    handleEditClick: function (e) {
+      e.preventDefault();
+      var $item = $(e.target);
+      $item.hide().siblings('.todo-edit-input').show();
+    },
+    /**
+     * when they change a todo item
+     * @param {object} event
+     */
+    handleEditSubmit: function (e) {
+      e.preventDefault();
+      // get the value of the input in this form and let TodoActions take it 
+      // from there
+      var todoEditInput = e.target.querySelector('.todo-edit-input');
+      var todoText = todoEditInput.value;
+      TodoActions.editTodo(this.props.todo.id, todoText);
+      // TODO: use async actions to only do this after async update is complete
+      $(todoEditInput).hide().siblings('.todo-text').show();
+    },
     render: function () {
       return(
         <li>
-          <a href="#" onClick={this.handleDeleteClick}>[Delete]</a>
-          <span>{this.props.todo.title}</span>
+          <form onSubmit={this.handleEditSubmit}>
+            <a href="#" onClick={this.handleDeleteClick}>[Delete]</a>
+            <input type="text" name="title" className="todo-edit-input" style={{display: "none"}} defaultValue={this.props.todo.title} />
+            <span className="todo-text" onClick={this.handleEditClick}>{this.props.todo.title}</span>
+          </form>
         </li>
       );
     }
